a081698dd7372f84ddcc88349b6bdd71e5f2715f
Josh <josh@usc-eduroam-upc-new95.usc.edu>
2019-08-01 08:57:49 -0700

    Created makeTree() function

--- scene.py
+++ scene.py
@@ -22,27 +22,32 @@ canvas.setCoords(0, 0, 500, 500)
 
 canvas.setBackground(color_rgb(27, 196, 247))
 
-#Tree Trunk
-trunk = Rectangle( Point(230, 0), Point(270, 300))
-trunk.setFill(color_rgb(79, 60, 32)  )
-trunk.draw(canvas)
 
-#Make a bunch of branches, and make them point in random directions
-for i in range(10):
-    branch = Line( Point( randint(230, 270), randint(230, 270)), Point( randint(100, 400), randint(100, 400)))
-    branch.setFill( color_rgb(79, 60, 32))
-    branch.setWidth( randint(3, 10))
-    branch.draw(canvas)
+
+def makeTree(canvas):
     
-#Make a million little leaf bunches and stick them on the tree
-for i in range(50):
-    leaf = Circle( Point( randint(100, 400), randint(100, 400)), randint(20, 50))
-    #Randomness element ensures that the tree has a healthy variety in the greens available
-    leaf.setFill( color_rgb( randint( 0, 20), randint(200, 255), randint(0, 40)))
-    leaf.draw(canvas)
+    #Tree Trunk
+    trunk = Rectangle( Point(230, 0), Point(270, 300))
+    trunk.setFill(color_rgb(79, 60, 32)  )
+    trunk.draw(canvas)
     
-#Because I'm really craving apples right now
-for i in range(15):
-    apple = Circle( Point( randint(100, 400), randint(100, 400)), randint(9, 15))
-    apple.setFill( color_rgb( randint( 200, 255), randint(0, 200), randint(0, 20)))
-    apple.draw(canvas)
+    #Make a bunch of branches, and make them point in random directions
+    for i in range(10):
+        branch = Line( Point( randint(230, 270), randint(230, 270)), Point( randint(100, 400), randint(100, 400)))
+        branch.setFill( color_rgb(79, 60, 32))
+        branch.setWidth( randint(3, 10))
+        branch.draw(canvas)
+        
+    #Make a million little leaf bunches and stick them on the tree
+    for i in range(50):
+        leaf = Circle( Point( randint(100, 400), randint(100, 400)), randint(20, 50))
+        #Randomness element ensures that the tree has a healthy variety in the greens available
+        leaf.setFill( color_rgb( randint( 0, 20), randint(200, 255), randint(0, 40)))
+        leaf.draw(canvas)
+        
+    #Because I'm really craving apples right now
+    for i in range(15):
+        apple = Circle( Point( randint(100, 400), randint(100, 400)), randint(9, 15))
+        apple.setFill( color_rgb( randint( 200, 255), randint(0, 200), randint(0, 20)))
+        apple.draw(canvas)
+makeTree(canvas)
