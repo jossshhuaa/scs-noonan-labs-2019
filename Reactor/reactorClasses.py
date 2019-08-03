7ba30d731917fccaa1f81e984fc6ac2c07df2494
Josh <josh@Joshs-MacBook-Air.local>
2019-08-03 13:05:40 -0700

    Fixed file interdependency

--- Reactor/reactorClasses.py
+++ Reactor/reactorClasses.py
@@ -10,12 +10,12 @@ class Reactor:
         self.heat = 0
         self.coolant_level = 100
         self.rod_insertion = 100
-        
+
     #Emergency shutdown
     def scram(self):
         self.rod_insertion = 100
         return True
-    
+
     #Move the control rods by some increment
     def moveControlRods(self, increment):
         if self.rod_insertion + increment > 100:
@@ -24,21 +24,19 @@ class Reactor:
             self.rod_insertion = 0
         else:
             self.rod_insertion += increment
-        
+
 #These are the numbers which get displayed; for example: 0100 MW of power, 1300 deg. C heat, etc.
 class DisplayNumber: #NYI
-    
     def __init__(self):
         None
 
-        
 class StatusBar:
 
     def __init__(self, level, top_left, bottom_right,):
         self.level = level #Number between 0 and 100
         self.top_left = top_left
         self.bottom_right = bottom_right
-        
+
         #This was the hardest part, it needed so much geometry
         #Draws 4 rectangles stacked on top of each other to show the status of reactor levels. I'll  color the top one black once the level drops below 75%, the third one black when level drops below 50%, etc.
         self.height = ( top_left.getY() - bottom_right.getY() )
@@ -46,7 +44,7 @@ class StatusBar:
         self.second_quad = Rectangle( Point( top_left.getX(), top_left.getY() - 2*(self.height/4)), Point( bottom_right.getX(), top_left.getY() - 3*(self.height/4)) )
         self.third_quad = Rectangle( Point( top_left.getX(), top_left.getY() - (self.height/4)), Point( bottom_right.getX(), top_left.getY() - 2*(self.height/4)) )
         self.top_quad = Rectangle( top_left, Point(bottom_right.getX(), top_left.getY() - (self.height/4)))
-        
+
     #Take in your window and 4 different color choices and draw your status bar
     def draw(self, win, color1, color2, color3, color4):
         self.bottom_quad.setFill( color1 )
@@ -57,13 +55,13 @@ class StatusBar:
         self.second_quad.draw(win)
         self.third_quad.draw(win)
         self.top_quad.draw(win)
-        
+
     def getLevel(self):
         return self.level
-    
-#Buttons!        
+
+#Buttons!
 class Button:
-    
+
     #Buttons know whether they've been pressed, their color, their center, what they look like
     def __init__(self, center, radius, color ):
         self.shape = 'Circle'
@@ -72,29 +70,29 @@ class Button:
         self.center = center
         self.radius = radius
         self.image = Circle(center, radius)
-        
+
     #Buttons know how to push themselves
     def push(self):
         self.pressed = True
         self.image.setFill('gray')
-        
+
     #Buttons know how to unpush themselves
-        
+
     def unpush(self):
         self.pressed = False
         self.image.setFill( self.color )
     #Draw self on win
-        
-    def draw(self, win):  
+
+    def draw(self, win):
         self.image.setFill( self.color)
         self.image.draw(win)
-        
+
     def checkIfPressed(self):
         return self.pressed
-    
+
 class SquareButton(Button):
-    
-    
+
+
     def __init__(self, top_left, bottom_right, color ):
         self.shape = 'Square'
         self.pressed = False
@@ -102,4 +100,3 @@ class SquareButton(Button):
         self.top_left = top_left
         self.bottom_right = bottom_right
         self.image = Rectangle(top_left, bottom_right)
-        
